48cf223ae39d38813df78928df0c4a73c93636b7
brishti55 <nushratkhan@yahoo.com>
2015-04-23 14:27:01 -0500

    sql query for top 3 product ig calculation

---
+++ final_project_top3prod.sql
@@ -0,0 +1,11 @@
+update top3prod
+set prodid = 1
+where prodid='B000GLRREU';
+
+update top3prod
+set prodid = 2
+where prodid='B000KUHFGM';
+
+update top3prod
+set prodid = 3
+where prodid='B000QSNYGI';
